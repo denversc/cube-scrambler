d3ff6694891242623ae6bc13835f68a277f0faf9
Denver Coneybeare <dconeybe@google.com>
2025-04-25 17:29:10 -0400

    test/cube.test.ts added tests for moveR

--- test/cube.test.ts
+++ test/cube.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import type { Suite } from "mocha";
 
-import { copyCube, CUBE_LENGTH, isCube, isEqualCube, solved } from "../src/cube";
+import { copyCube, Cube, CUBE_LENGTH, isCube, isEqualCube, moveR, solved } from "../src/cube";
 import { getRandomElementFrom, getRandomIndexOf } from "../src/random";
 import { nonArrayValues, randomColor, randomColorArray, randomCube } from "./cube.testing";
 import { initializeRngForEachTest, rng } from "./random.testing";
@@ -130,4 +130,103 @@ describe("cube.test.ts [g7em876hy4]", function (this: Suite) {
       }
     });
   });
+
+  describe("moveR() [b37f8k4pkn]", () => {
+    it("should caused expected state on solved cube", () => {
+      const cube = solved();
+
+      moveR(cube);
+      // prettier-ignore
+      expect(cube, "after 1 move").to.deep.equal([
+        "green", "green", "yellow", "green", GREEN, "yellow", "green", "green", "yellow",
+        "orange", "orange", "orange", "orange", ORANGE, "orange", "orange", "orange", "orange",
+        "blue", "blue", "white", "blue", BLUE, "white", "blue", "blue", "white",
+        "red", "red", "red", "red", RED, "red", "red", "red", "red",
+        "white", "white", "green", "white", "white", "green", "white", "white", "green",
+        "yellow", "yellow", "blue", "yellow", YELLOW, "blue", "yellow", "yellow", "blue",
+      ]);
+
+      moveR(cube);
+      // prettier-ignore
+      expect(cube, "after 2 moves").to.deep.equal([
+        "green", "green", "blue", "green", GREEN, "blue", "green", "green", "blue",
+        "orange", "orange", "orange", "orange", ORANGE, "orange", "orange", "orange", "orange",
+        "blue", "blue", "green", "blue", BLUE, "green", "blue", "blue", "green",
+        "red", "red", "red", "red", RED, "red", "red", "red", "red",
+        "white", "white", "yellow", "white", WHITE, "yellow", "white", "white", "yellow",
+        "yellow", "yellow", "white", "yellow", YELLOW, "white", "yellow", "yellow", "white",
+      ]);
+
+      moveR(cube);
+      // prettier-ignore
+      expect(cube, "after 3 moves").to.deep.equal([
+        "green", "green", "white", "green", GREEN, "white", "green", "green", "white",
+        "orange", "orange", "orange", "orange", ORANGE, "orange", "orange", "orange", "orange",
+        "blue", "blue", "yellow", "blue", BLUE, "yellow", "blue", "blue", "yellow",
+        "red", "red", "red", "red", "red", RED, "red", "red", "red",
+        "white", "white", "blue", "white", WHITE, "blue", "white", "white", "blue",
+        "yellow", "yellow", "green", "yellow", YELLOW, "green", "yellow", "yellow", "green",
+      ]);
+
+      moveR(cube);
+      expect(cube, "after 4 moves").to.deep.equal(solved());
+    });
+
+    it("should caused expected state on random cube 1", () => {
+      // prettier-ignore
+      const cube: Cube = [
+        "red", "white", "blue", "white", GREEN, "red", "green", "blue", "red",
+        "orange", "green", "blue", "orange", ORANGE, "red", "green", "blue", "white",
+        "green", "blue", "red", "white", BLUE, "white", "blue", "blue", "orange",
+        "blue", "white", "orange", "red", RED, "blue", "green", "green", "yellow",
+        "yellow", "yellow", "yellow", "white", WHITE, "green", "white", "orange", "orange",
+        "blue", "white", "blue", "red", YELLOW, "white", "yellow", "blue", "orange",
+      ];
+      const initialCube = copyCube(cube);
+
+      moveR(cube);
+      // prettier-ignore
+      expect(cube, "after 1 move").to.deep.equal([
+        "red", "white", "blue", "white", GREEN, "white", "green", "blue", "orange",
+        "orange", "green", "blue", "orange", ORANGE, "red", "green", "blue", "white",
+        "green", "blue", "yellow", "white", BLUE, "green", "blue", "blue", "orange",
+        "green", "red", "blue", "green", RED, "white", "yellow", "blue", "orange",
+        "yellow", "yellow", "blue", "white", WHITE, "red", "white", "orange", "red",
+        "blue", "white", "red", "red", YELLOW, "white", "yellow", "blue", "orange",
+      ]);
+
+      moveR(cube);
+      // prettier-ignore
+      expect(cube, "after 2 moves").to.deep.equal([
+        "red", "white", "red", "white", GREEN, "white", "green", "blue", "orange",
+        "orange", "green", "blue", "orange", ORANGE, "red", "green", "blue", "white",
+        "green", "blue", "blue", "white", BLUE, "red", "blue", "blue", "red",
+        "yellow", "green", "green", "blue", RED, "red", "orange", "white", "blue",
+        "yellow", "yellow", "blue", "white", WHITE, "white", "white", "orange", "orange",
+        "blue", "white", "yellow", "red", YELLOW, "green", "yellow", "blue", "orange",
+      ]);
+
+      moveR(cube);
+      // prettier-ignore
+      expect(cube, "after 2 moves").to.deep.equal([
+        "red", "white", "yellow", "white", GREEN, "green", "green", "blue", "orange",
+        "orange", "green", "blue", "orange", ORANGE, "red", "green", "blue", "white",
+        "green", "blue", "blue", "white", BLUE, "white", "blue", "blue", "orange",
+        "orange", "blue", "yellow", "white", RED, "green", "blue", "red", "green",
+        "yellow", "yellow", "red", "white", WHITE, "white", "white", "orange", "orange",
+        "blue", "white", "blue", "red", YELLOW, "red", "yellow", "blue", "red",
+      ]);
+
+      moveR(cube);
+      expect(cube, "after 4 moves").to.deep.equal(initialCube);
+    });
+  });
 });
+
+// These constants help improve readability for the immutable rows
+const GREEN = "green";
+const RED = "red";
+const ORANGE = "orange";
+const BLUE = "blue";
+const WHITE = "white";
+const YELLOW = "yellow";
