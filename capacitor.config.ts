2d48f047732628ed5278189a018321adc31d3f42
Denver Coneybeare <dconeybe@google.com>
2025-09-02 01:34:07 -0400

    configure capacitor to make an android app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cubescrambler.app',
+  appName: 'Cube Scrambler',
+  webDir: 'build/html',
+  bundledWebRuntime: false,
+};
+
+export default config;
