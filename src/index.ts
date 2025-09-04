9fef638e1a2f79b3a25be77358087b45a28b618e
Denver Coneybeare <dconeybe@google.com>
2025-09-04 02:51:29 -0400

    src/index.ts: show optimized moves

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { type Cube, type Face, Faces, getFace, type Move, solvedCube } from "./cube";
 import { draw } from "./draw";
+import { calculateOptimizedMoves } from "./optimize";
 import { generateScramble } from "./scramble";
 import { solveWhiteCross } from "./solve_white_cross";
 import { transform } from "./transform";
@@ -16,9 +17,22 @@ function runScramble() {
   const cube = solvedCube();
   const scramble = generateScramble();
   transform(cube, scramble);
+  console.log(`scramble: ${scramble.join(" ")} ` + `(length=${scramble.length})`);
+
   const whiteCrossSolveMoves = solveWhiteCross(cube);
-  console.log(scramble.join(", "));
-  console.log(whiteCrossSolveMoves.join(", "));
+  console.log(
+    `whiteCrossSolveMoves: ${whiteCrossSolveMoves.join(" ")} ` +
+      `(length=${whiteCrossSolveMoves.length})`,
+  );
+
+  const unoptimizedMoves = [...scramble, ...whiteCrossSolveMoves];
+  console.log(
+    `unoptimizedMoves: ${unoptimizedMoves.join(" ")} ` + `(length=${unoptimizedMoves.length})`,
+  );
+
+  const optimizedMoves = calculateOptimizedMoves(unoptimizedMoves);
+  console.log(`optimizedMoves: ${optimizedMoves.join(" ")} ` + `(length=${optimizedMoves.length})`);
+
   draw(cube);
 }
 
